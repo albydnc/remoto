eff0ef8a40471e7cb35999cba7dcbee872c3e3ad
Alberto Perro <alberto.perro@cern.ch>
2025-06-06 01:17:19 +0200

    add expansion support to web ui

--- api_tests/server.py
+++ api_tests/server.py
@@ -2,7 +2,7 @@
 IoT Device API Server with Flask
 
 This script creates a simple API server for managing and interacting with an IoT device
-using Flask. The server provides endpoints for real-time data retrieval, device configuration, 
+using Flask. The server provides endpoints for real-time data retrieval, device configuration,
 and MQTT publishing simulation. It also serves static web pages for device monitoring and configuration.
 
 Features:
@@ -12,48 +12,107 @@ Features:
 - Simulated MQTT publish endpoint.
 """
 
-
 from flask import Flask, jsonify, request
 
 # Mock telemetry data for the IoT device
 data = {
-    "deviceId": "REMOTO052",  # Unique ID for the device
-    "mqttConnected": "true",  # Indicates MQTT connection status
-    "lastPublish": 125,  # Time in seconds since the last telemetry publish
-    "NTP": 1736370059,
-    "inputs": {  # Inputs with their current values and types
-        "I1": {"value": True, "type": True},
-        "I2": {"value": True, "type": True},
-        "I3": {"value": True, "type": True},
-        "I4": {"value": True, "type": True},
-        "I5": {"value": True, "type": True},
-        "I6": {"value": True, "type": True},
-        "I7": {"value": 0.05, "type": False},  # Analog input example
-        "I8": {"value": 6.5, "type": False},   # Analog input example
+    "deviceId": "OPTA_WIFI",
+    "mqttConnected": False,
+    "NTP": 1749162624,
+    "lastPublish": 67,
+    "inputs": {
+        "I1": {"value": 0, "type": True},
+        "I2": {"value": 0, "type": True},
+        "I3": {"value": 0, "type": True},
+        "I4": {"value": 0, "type": True},
+        "I5": {"value": 0, "type": True},
+        "I6": {"value": 0, "type": True},
+        "I7": {"value": 0.005882519, "type": False},
+        "I8": {"value": 0, "type": False},
     },
-    "outputs": {  # Outputs with their current states
-        "O1": True,
-        "O2": True,
-        "O3": False,
-        "O4": True,
-    }
+    "outputs": {"O1": 0, "O2": 0, "O3": 0, "O4": 0},
+    "expansions": {
+        "E1": {
+            "type": "D1608E",
+            "inputs": {
+                "I1": {"value": False, "volt": 0},
+                "I2": {"value": False, "volt": 0},
+                "I3": {"value": False, "volt": 0},
+                "I4": {"value": False, "volt": 0},
+                "I5": {"value": False, "volt": 0},
+                "I6": {"value": False, "volt": 0},
+                "I7": {"value": False, "volt": 0},
+                "I8": {"value": False, "volt": 0},
+                "I9": {"value": False, "volt": 0},
+                "I10": {"value": False, "volt": 0},
+                "I11": {"value": False, "volt": 0},
+                "I12": {"value": False, "volt": 0},
+                "I13": {"value": False, "volt": 0},
+                "I14": {"value": False, "volt": 0},
+                "I15": {"value": False, "volt": 0},
+                "I16": {"value": False, "volt": 0},
+            },
+            "outputs": {
+                "O1": {"value": False},
+                "O2": {"value": False},
+                "O3": {"value": False},
+                "O4": {"value": False},
+                "O5": {"value": False},
+                "O6": {"value": False},
+                "O7": {"value": False},
+                "O8": {"value": False},
+            },
+        },
+        "E2": {
+            "type": "D1608S",
+            "inputs": {
+                "I1": {"value": False, "volt": 0},
+                "I2": {"value": False, "volt": 0},
+                "I3": {"value": False, "volt": 0},
+                "I4": {"value": False, "volt": 0},
+                "I5": {"value": False, "volt": 0},
+                "I6": {"value": False, "volt": 0},
+                "I7": {"value": False, "volt": 0},
+                "I8": {"value": False, "volt": 0},
+                "I9": {"value": False, "volt": 0},
+                "I10": {"value": False, "volt": 0},
+                "I11": {"value": False, "volt": 0},
+                "I12": {"value": False, "volt": 0},
+                "I13": {"value": False, "volt": 0},
+                "I14": {"value": False, "volt": 0},
+                "I15": {"value": False, "volt": 0},
+                "I16": {"value": False, "volt": 0},
+            },
+            "outputs": {
+                "O1": {"value": False},
+                "O2": {"value": False},
+                "O3": {"value": False},
+                "O4": {"value": False},
+                "O5": {"value": False},
+                "O6": {"value": False},
+                "O7": {"value": False},
+                "O8": {"value": False},
+            },
+        }
+    },
 }
+
 
 # Default configuration for the IoT device
 config = {
     "deviceId": "OPTA_WIFI",  # Device ID
     "deviceIpAddress": "192.168.1.231",  # IP Address
     "dhcp": True,  # Indicates if DHCP is enabled
-    "preferWifi": True, # Indicates if the WiFi connection is prefered
-    "ssid": "MYSSID", # Example SSID
-    "wifiPass": "SSID Password", # Example SSID Password
+    "preferWifi": True,  # Indicates if the WiFi connection is prefered
+    "ssid": "MYSSID",  # Example SSID
+    "wifiPass": "SSID Password",  # Example SSID Password
     "timeServer": "TimeServer",
     "mqtt": {  # MQTT broker configuration
         "server": "public.cloud.shiftr.io",
         "port": 1883,
         "user": "public",
         "password": "public",
-        "updateInterval": 300  # Telemetry update interval in seconds
+        "updateInterval": 300,  # Telemetry update interval in seconds
     },
     "inputs": {  # Pin configurations for the inputs
         "I1": 1,
@@ -63,44 +122,50 @@ config = {
         "I5": 1,
         "I6": 1,
         "I7": 0,
-        "I8": 0
-    }
+        "I8": 0,
+    },
 }
 
 # Flask app initialization with a static folder for serving web pages
 api = Flask(__name__, static_folder="web/")
 
+
 # Serve the root HTML page
-@api.route('/', methods=['GET'])
+@api.route("/", methods=["GET"])
 def get_root():
     return api.send_static_file("root.html")
 
+
 # Serve the device configuration HTML page
-@api.route('/device', methods=['GET'])
+@api.route("/device", methods=["GET"])
 def get_device():
     return api.send_static_file("config.html")
 
+
 # Endpoint for real-time data retrieval
-@api.route('/data', methods=['GET'])
+@api.route("/data", methods=["GET"])
 def get_realtime():
     return jsonify(data)
 
+
 # Endpoint for retrieving or updating device configuration
-@api.route('/config', methods=['GET', 'POST'])
+@api.route("/config", methods=["GET", "POST"])
 def config_endpoint():
     global config
-    if request.method == 'GET':  # Return the current configuration
+    if request.method == "GET":  # Return the current configuration
         return jsonify(config)
-    elif request.method == 'POST':  # Update the configuration with the provided JSON
+    elif request.method == "POST":  # Update the configuration with the provided JSON
         print(request.json)  # Log the received configuration
         config = request.json
         return jsonify({"message": "Configuration updated successfully!"})
 
+
 # Simulated endpoint for MQTT publishing
-@api.route('/send', methods=['GET'])
+@api.route("/send", methods=["GET"])
 def get_send():
     return jsonify({"message": "MQTT published successfully!"})
 
+
 # Start the Flask server
-if __name__ == '__main__':
+if __name__ == "__main__":
     api.run()
